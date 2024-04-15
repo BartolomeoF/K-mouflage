023432a7600c0bc653a9a454727b2edc4dee52c1
Bartolomeo <b.fiorini@qmul.ac.uk>
2024-04-15 18:51:28 +0100

    Base version

---
+++ my_style.py
@@ -0,0 +1,55 @@
+axes.grid: 1
+axes.labelsize: 14.0
+axes.linewidth: 1.7
+axes.prop_cycle: cycler('color', ['1f77b4', 'ff7f0e', '2ca02c', 'd62728', '9467bd', '8c564b', 'e377c2', '7f7f7f', 'bcbd22', '17becf'])
+axes.titlesize: 20.0
+    
+figure.edgecolor: 'white'
+figure.facecolor: 'white'
+figure.figsize: 8, 5
+font.size: 14.0
+# grid.alpha: 0.3
+# grid.color: black
+# grid.linestyle: --
+# grid.linewidth: 0.5
+
+legend.fancybox: True
+legend.fontsize: 14.0
+    
+lines.dash_capstyle: butt
+lines.linewidth: 2
+lines.markeredgewidth: 1.0
+lines.markersize: 5.0
+lines.dashed_pattern : 5, 3
+lines.dashdot_pattern : 3, 5, 1, 5
+lines.dotted_pattern : 1, 3
+lines.scale_dashes: True
+    
+savefig.bbox: tight
+savefig.format: pdf
+savefig.transparent: False
+text.usetex: False
+xtick.labelsize: 12.0
+xtick.major.size: 6.0
+xtick.major.width: 1.7
+xtick.minor.size: 4.0
+xtick.minor.width: 1.4
+ytick.labelsize: 12.0
+ytick.major.size: 6.0
+ytick.major.width: 1.7
+ytick.minor.size: 4.0
+ytick.minor.width: 1.4
+xtick.top:True
+ytick.right:True
+xtick.direction:in
+ytick.direction:in
+    
+### Color
+# image.cmap: plasma
+
+### GRIDS
+grid.color       :   k       # grid color
+grid.linestyle   :   --       # dashed
+grid.linewidth   :   0.5     # in points
+grid.alpha       :   0.5     # transparency, between 0.0 and 1.0
+          
